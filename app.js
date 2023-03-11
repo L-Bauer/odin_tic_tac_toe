ac7ba732662948fe8e4c8b3f1208a84f841689ea
Lucas Bauer <lucas.d.bauer95@gmail.com>
2023-03-10 19:47:43 -0600

    Learning how to use factory functions"

--- app.js
+++ app.js
@@ -1,45 +1,25 @@
 
 
-const Board = (() => {
-  const rows = 3;
-  const columns = 3;
-  const board = [];
+const gameBoard = (() => {
+  let _board = new Array(9)
 
-  // Create a 2d array that will represent the state of the game board
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(0);
-    }
+  function displayBoard () {
+    console.log(_board);
   }
 
-  // This will be the method of getting the entire board that our
-  // UI will eventually need to render it.
-  const getBoard = () => board;
-
-  const placeToken = (spot, player) => {
-    
+  function updateBoard (position, token) {
+    _board[position] = token
+    displayBoard();
   }
 
-  // This method will be used to print our board to the console.
-  // It is helpful to see what the board looks like after each turn as we play,
-  // but we won't need it after we build our UI
-  const printBoard = () => {
-    console.log(board);
-  };
-
-  // Here, we provide an interface for the rest of our
-  // application to interact with the board
-  return { printBoard, getBoard };
+  return { displayBoard, updateBoard };
+  
 })();
 
 
-const Player = (name, token) => {
+const gameLogic = (() => {
 
-  const getName = () => name;
-  const getToken = () => token;
+  gameBoard.displayBoard();
 
-  return { getName, getToken }
-};
+})();
 
-console.log(Board.getBoard())
